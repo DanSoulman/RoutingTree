17f6cf27498d12d2c7c2381f7049d9382c08cf7b
DanSoulman <daniel.coleman1@mycit.ie>
2018-12-11 12:15:17 +0000

    Basic Hello broadcasr from Beacon to Reciever

---
+++ Beacon.c
@@ -0,0 +1,51 @@
+//DAN Coleman Broadcast Sample 
+#include "contiki.h"
+#include "net/rime/rime.h"
+#include "random.h"
+#include <stdio.h>
+
+/*---------------------------------------------------------------------------*/
+PROCESS(example_broadcast_process, "Broadcast example");
+AUTOSTART_PROCESSES(&example_broadcast_process);
+/*---------------------------------------------------------------------------*/
+
+//executes when the collector gets broadcast						           
+static void broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from){
+}
+
+//Tells collector which function to call when it gets a broadcast
+static const struct broadcast_callbacks broadcast_call = { broadcast_recv };
+static struct broadcast_conn broadcast;
+
+
+//Runs a thread that loops a broadcast to the other nodes
+PROCESS_THREAD(example_broadcast_process, ev, data)
+{
+   //Creates ETimer
+  static struct etimer et;
+   PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
+   PROCESS_BEGIN();
+
+   //Opens Broad and Unicast on ports 146 and 140
+   broadcast_open(&broadcast, 146, &broadcast_call);
+
+   struct node_info packet;
+   int sequence_counter = 0;
+   int wipe_counter = 0;
+
+   while(1) {
+     // Delay for 4 seconds
+     etimer_set(&et, CLOCK_SECOND * 4); //+ random_rand() % (CLOCK_SECOND * 4));
+     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+
+	  //Sends out a broadcast saying hello.
+     packetbuf_copyfrom("Hello", 6);
+     broadcast_send(&broadcast);
+
+   }
+
+   PROCESS_END();
+
+}
+
+/*---------------------------------------------------------------------------*/
---
+++ Receivers.c
@@ -0,0 +1,40 @@
+//DAN Coleman unicast Sample 
+//This is for the different versions of contiki
+#include "contiki.h"
+#include "net/rime/rime.h"
+#include <stdio.h>
+
+PROCESS(example_unicast_process, "Example unicast");
+AUTOSTART_PROCESSES(&example_unicast_process);
+
+
+
+static void broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from){
+  printf("broadcast message received from %d.%d: '%s'\n",
+         from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
+
+  linkaddr_t destination;
+
+}
+
+ //lets collector know which function to do when broadcast is received
+ static const struct broadcast_callbacks broadcast_call = { broadcast_recv };
+ static struct broadcast_conn broadcast;
+
+PROCESS_THREAD(example_unicast_process, ev, data){
+
+  PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
+  PROCESS_BEGIN();
+
+  //Opens Broad and Unicast on ports 146 and 140
+  broadcast_open(&broadcast, 146, &broadcast_call);
+
+
+    //While--------------------
+  while(1) {
+
+  }
+
+  //Ends Process Thread
+  PROCESS_END();
+}
