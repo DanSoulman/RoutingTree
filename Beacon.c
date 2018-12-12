77de25f8931dbfd110ad09b4fea4e877b83079a8
Dan Coleman <32960483+DanSoulman@users.noreply.github.com>
2018-12-12 22:34:08 +0000

    Commented

--- Beacon.c
+++ Beacon.c
@@ -1,4 +1,4 @@
-//DAN Coleman Broadcast Sample 
+//DAN Coleman Beacon for Assignment 2
 #include "contiki.h"
 #include "net/rime/rime.h"
 #include "random.h"
@@ -34,7 +34,8 @@ PROCESS_THREAD(example_broadcast_process, ev, data)
    //Opens Broad on port 146
    broadcast_open(&broadcast, 146, &broadcast_call);
 
-   struct node_info packet; //Creates instance of node info called packet
+  //Creates instance of node info called packet
+   struct node_info packet; 
 
    //Instansiates counters
    static int sequence_counter = 0;
@@ -44,28 +45,37 @@ PROCESS_THREAD(example_broadcast_process, ev, data)
      etimer_set(&et, CLOCK_SECOND * 4); //+ random_rand() % (CLOCK_SECOND * 4));
      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 
+    //Sets the hop to zero to count how many hops to each node
      packet.hop = 0;
+     //Sets sequence number to which sequence its on (resets after 15)
      packet.sequence_number = sequence_counter;
+     //Indicates it doesn't need to wipe the system yet
      packet.wipe_node = false;
 
     //Once wipe limit has been reached
     if(packet.sequence_number > 15){
+      //Indicates the system is to be wiped
        packet.wipe_node = true;
        printf("Wiping"); //For testing
      }
 
-    else //Shows message sent
-      printf("Broadcast message sent\n");
+    else //Shows that broadcast has been sent out, expect the nodes to start returning info
+      printf("Broadcast message sent\n"); 
 
+    /*copies data from the packet struct
+    the data copied is the size of node_info */    
     packetbuf_copyfrom(&packet, sizeof(struct node_info));
+    //Sends the broadcast specified above
     broadcast_send(&broadcast);
 
-    if(sequence_counter > 15){ //If it goes past the limit it wipes everything
+    //If it goes past the limit (15) it wipes everything
+    if(sequence_counter > 15){
       sequence_counter = 0;
       packet.wipe_node = false;
      }
 
-    else{ //Otherwise it increments
+    //Otherwise it increments the sequence number and starts again
+    else{ 
       sequence_counter += 1;
      }
 
--- Receivers.c
+++ Receivers.c
@@ -1,12 +1,13 @@
-//DAN Coleman unicast Sample 
-//This is for the different versions of contiki
+//DAN Coleman Reciever Assignment 2
 #include "contiki.h"
 #include "net/rime/rime.h"
 #include <stdio.h>
 #include "stdbool.h"
 
+//---------------------------------------------
 PROCESS(example_unicast_process, "Reciever");
 AUTOSTART_PROCESSES(&example_unicast_process);
+//---------------------------------------------
 
 //Struct that holds the node info
 struct node_info{
@@ -15,59 +16,81 @@ struct node_info{
   bool wipe_node;
 };
 
-static int sequence_number = -1; //Static sequence number for working out parents 
+//Static sequence number for working out parents 
+static int sequence_number = -1; 
 
 static struct broadcast_conn broadcast;
 
+//address of the parent node 
 static linkaddr_t sender;
 
+//Boolean to show if node has a parent 
 bool parentSet = false;
 
+//Initializes Struct with the node information named packet
 static struct node_info packet;
 
+//What to do when node recieves a broadcast
 static void broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from){
 
+  //Gets a pointer (store) to the data from the packet
   struct node_info *store = packetbuf_dataptr();
-
+  //Set the node info (packets) information to the information from the packet (store)
   packet.sequence_number = store->sequence_number;
   packet.hop = store->hop;
   packet.wipe_node = store->wipe_node;
+ //Increment hop count by one since we hopped to this node
   packet.hop +=1 ;
 
+  //If the packet was to wipe (16th in sequence)
   if(packet.wipe_node == true){
+    //Set sequence number to -1 to start again
     sequence_number = -1;
+    //No Parent set for each node
     parentSet = false;
-
+    //The address of the parent is null
     sender.u8[0] = NULL;
     sender.u8[1] = NULL;
 
+    //Tells us this is a wiping node 
+    //(known issue: The wipe doesn't seem to reach all nodes)
     printf("Wiping node");
 
+    /*copies data from the packet struct
+    the data copied is the size of node_info and sends broadcast */    
     packetbuf_copyfrom(&packet, sizeof(struct node_info));
     broadcast_send(&broadcast);
   }
-  else{
+  //If not wiping
+  else{ 
+    //And doesn't have a parent
     if(parentSet == false){
+      //Sets the parents info 
       sender.u8[0] = from->u8[0];
       sender.u8[1] = from->u8[1];
-
+      //Sequence number is packet sequence no. 
       sequence_number = packet.sequence_number;
 
+      //Prints the information
       printf("Seq: %d, Hop: %d\n", packet.sequence_number, packet.hop);
-
+      //Parent now set, won't enter this loop again until boolean set to false 
       parentSet = true;
 
+      /*copies data from the packet struct
+    the data copied is the size of node_info and sends broadcast */
       packetbuf_copyfrom(&packet, sizeof(struct node_info));
       broadcast_send(&broadcast);
 
     }
-    
+    //If not wiping anf it has a parent
     else if(packet.sequence_number > sequence_number && ((from->u8[0] == sender.u8[0]) && (from->u8[1] == sender.u8[1]))){
-
+      //Sequence no. is number of packt sent 
       sequence_number = packet.sequence_number;
-
+      //Prints info on packet 
       printf("Seq: %d, Hop: %d\n", packet.sequence_number, packet.hop);
 
+    /*copies data from the packet struct
+    the data copied is the size of node_info and sends broadcast */
       packetbuf_copyfrom(&packet, sizeof(struct node_info));
       broadcast_send(&broadcast);
     }
@@ -75,17 +98,17 @@ static void broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from){
 
 }
 
- //lets collector know which function to do when broadcast is received
+ //lets node know which function to do when broadcast is received
  static const struct broadcast_callbacks broadcast_call = { broadcast_recv };
 
 
 PROCESS_THREAD(example_unicast_process, ev, data){
-
+  //Runs thread
   PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
   PROCESS_BEGIN();
 
 
-  //Opens Broad and Unicast on ports 146 and 140
+  //Opens Broad port 146 
   broadcast_open(&broadcast, 146, &broadcast_call);
 
   //Ends Process Thread
